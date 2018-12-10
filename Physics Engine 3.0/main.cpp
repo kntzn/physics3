811f4d321a57ead0461e206ba2b75eb58755770a
eldardamirov <eldardamirov75@mail.ru>
2018-12-10 23:53:54 +0300

    Initial Commit

---
+++ Physics Engine 3.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Physics Engine 3.0
+//
+//  Created by Эльдар Дамиров on 10.12.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
