9f43ea59345a2d0a79970be3d63cc38bc2c3d86f
kntzn <kntznart@gmail.com>
2018-12-11 00:19:22 +0300

    [Ini]: created vs proj

--- Physics Engine 3.0/main.cpp
+++ Physics Engine 3.0/main.cpp
@@ -1,9 +1,12 @@
 //
 //  main.cpp
-//  Physics Engine 3.0
+//  test
 //
 //  Created by Эльдар Дамиров on 10.12.2018.
 //  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+//  Improved by kntzn on 10.12.2018.
+//  Copyright © 2018 kntzn. All rights reserved.
 //
 
 #include <iostream>
@@ -11,6 +14,6 @@
 int main() 
     {
     
-    
+    std::cout << "АГА, ПОПАЛСЯ!1!\n";
     return 0;
     }
