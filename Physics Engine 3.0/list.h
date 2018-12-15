2258d1b3c2639da2e88facd9560fce10de6f3d6b
eldardamirov <eldardamirov75@mail.ru>
2018-12-15 18:43:19 +0300

    Basic container modifiers added.

--- Physics Engine 3.0/list.h
+++ Physics Engine 3.0/list.h
@@ -5,6 +5,8 @@
 #include <math.h>
 
 
+//#define prsVal // preserves and returns value during pop operations;
+
 template <typename dataType> struct Node
     {
     dataType value;
@@ -17,7 +19,7 @@ template <typename dataType> struct Node
         {
         }
         
-    Node ( dataType value, Node* left, Node* right ):
+    Node ( dataType value = NULL, Node* left = nullptr, Node* right = nullptr ):
         value ( value ),
         prev ( left ),
         next ( right )
@@ -30,30 +32,89 @@ template <typename dataType> class list
     {
     private:
         size_t currentLength = 0;
-        size_t wholeSpaceReserved = 0;
+        size_t reservedCapacity = 0;
         
         Node <dataType>* front = nullptr;
-        Node <dataType>* tail = nullptr;
+        Node <dataType>* back = nullptr;
         
         
         // ----Memory-safety precautions
         dataType poisonValue = dynamic_cast <dataType> ( NULL );
-        Node <dataType> borderBack  = NULL;
-        Node <dataType> borderFront = NULL;
+        Node <dataType>* borderBack  = nullptr;
+        Node <dataType>* borderFront = nullptr;
         
         // ----
         
     public:
+    
         // Constructors and destructor
         list ( size_t reservedCapacity = 0 ): 
-            borderBack ( poisonValue, &borderFront, this ),
-            borderFront ( poisonValue, this, &borderBack )
+            borderBack ( poisonValue, back, this ),
+            borderFront ( poisonValue, this, front ),
+            front ( poisonValue, borderFront, back ),
+            back ( poisonValue, borderBack, front )
             {
-            
+            this->reservedCapacity = reservedCapacity;
             }
         
         ~list ();
 
+        
+        void push_back ( dataType value )
+            {
+            Node <dataType>* newElement ( value, back, borderBack );
+            back->next = newElement;
+            
+            back = newElement;
+            }
+        
+        #ifdef prsVal
+        dataType pop_back()
+        #else
+        void pop_back()
+        #endif
+            {
+            #ifdef prsVal
+            dataType preservedValue = back->value;
+            #endif 
+            
+            back = back->prev;
+            delete back->next;
+            
+            back->next = borderBack;
+            
+            #ifdef prsVal
+            return preservedValue;
+            #endif 
+            }
+        
+        void push_front ( dataType value )
+            {
+            Node <dataType>* newElement ( value, borderFront, front );
+            front->prev = newElement;
+            
+            prev = newElement;
+            }
+            
+        #ifdef prsVal
+        dataType pop_front()
+        #else
+        void pop_front()
+        #endif
+            {
+            #ifdef prsVal
+            dataType preservedValue = front->value;
+            #endif 
+            
+            front = back->next;
+            delete front->prev;
+            
+            front->prev = borderFront;
+            
+            #ifdef prsVal
+            return preservedValue;
+            #endif 
+            }
         
 
         // TODO:
@@ -62,11 +123,10 @@ template <typename dataType> class list
         // end
 
         // Modifiers
-        //void push_back (dataType value);
-
-        //bool pop_back ();
-        //bool push_front (dataType value);
-        //bool pop_front ();
+        //void push_back (dataType value); [DONE]
+        //bool pop_back (); [DONE]
+        //bool push_front (dataType value); [DONE]
+        //bool pop_front (); [DONE]
         //bool insert (dataType value, sarrln index);
         //bool erase (sarrln index);
         
