bdb730ae70cba9a93a00e0d6399110ce82664d38
vovayrovsky <vova@txlib.ru>
2018-12-21 14:38:05 +0300

    Add class Error

--- ErrorController/ErrorController.h
+++ ErrorController/ErrorController.h
@@ -5,9 +5,39 @@
 
 #include "defstreams.h"
 
+using namespace std;
+
+class Error
+    {
+    public:
+
+    size_t ErrorId;
+    string message;
+
+    Error():
+        ErrorId (0),
+        message()
+        {}
+
+
+    Error(size_t SEI):
+        ErrorId (SEI),
+        message()
+        {}
+
+
+    Error(size_t SEI, string SEM):
+        ErrorId (SEI),
+        message (SEM)
+        {}
+    };
+
 class ErrorController
     {
+    vector Errors <Error>;
 
+    vector ErrorHandlers <Error>;
     };
+
 
 
