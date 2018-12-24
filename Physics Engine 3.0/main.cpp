18aee954e439d3118a85217db4b5b23ab823d403
kntzn <kntznart@gmail.com>
2018-12-24 09:44:46 +0300

    [Add]: evaluate ()

--- Physics Engine 3.0/main.cpp
+++ Physics Engine 3.0/main.cpp
@@ -93,11 +93,11 @@ void updateAllPairs (darray <Body*> &all_objects,
         object_pairs [i]->update (all_objects);
     }
 
-Vectord acceleration (State state,
-                      darray <Body*> &all_objects,
-                      darray <Pair*> &object_pairs,
-                      Body* body)
-    {
+State acceleration (State state,
+                    darray <Body*> &all_objects,
+                    darray <Pair*> &object_pairs,
+                    Body* body)
+{
     State initial = *(body->getState ());
 
     state.a = Vectord (0, 0);
@@ -109,9 +109,64 @@ Vectord acceleration (State state,
             object_pairs [i]->getRight () == body)
             object_pairs [i]->update (all_objects);
 
-    Vectord output = body->getState ()->a;
+    State output = *body->getState ();
     
     *(body->getState ()) = initial;
     
     return output;
     }
+
+Derivative evaluate (Body* body,
+                     const State & initial,
+                     const PHYSENG_DATA_TYPE dt,
+                     const Derivative & d,
+                     darray <Body*> &all_objects,
+                     darray <Pair*> &object_pairs)
+    {
+    State new_state;
+    new_state.r = initial.r + d.dr*dt;
+    new_state.v = initial.v + d.dv*dt;
+
+    new_state.angle = initial.angle + d.dr*dt;
+    new_state.omega = initial.omega + d.dv*dt;
+
+    State accelerated_state = acceleration (new_state, all_objects, object_pairs, body);
+    new_state.a    = accelerated_state.a;
+    new_state.aAng = accelerated_state.aAng;
+
+    Derivative output;
+    output.dr = new_state.v;
+    output.dAng = new_state.omega;
+
+    output.dv = new_state.a;
+    output.dw = new_state.aAng;
+
+    return output;
+    }
+
+/*
+void MPoint::integrateRK4 (PHYSENG_DATA_TYPE dt)
+{
+Derivative a, b, c, d;
+
+a = evaluate (state, 0.0, Derivative ());
+b = evaluate (state, dt * 0.5, a);
+c = evaluate (state, dt * 0.5, b);
+d = evaluate (state, dt, c);
+
+Vectord drdt = Vectord (a.dr + Vectord ((b.dr + c.dr) * 2.0) + d.dr) / 6.0;
+Vectord dvdt = Vectord (a.dv + Vectord ((b.dv + c.dv) * 2.0) + d.dv) / 6.0;
+
+state.r += Vectord (drdt * dt);
+state.v += Vectord (dvdt * dt);
+state.a = Vectord (0, 0);
+}
+
+void MPoint::integrateEUL (PHYSENG_DATA_TYPE dt)
+{
+state.v += Vectord (state.a * dt);
+state.r += Vectord (state.v * dt);
+state.a = Vectord (0, 0);
+}
+
+*/
